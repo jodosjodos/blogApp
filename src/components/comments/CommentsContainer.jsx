ef657f64709c5cccff2b78ff314d27ffd986bbd1
jodosjodos <jeandedieu2030@gmail.com>
2023-06-03 21:45:52 +0200

    delete functionality

--- src/components/comments/CommentsContainer.jsx
+++ src/components/comments/CommentsContainer.jsx
@@ -1,13 +1,11 @@
 import { useEffect, useState } from "react";
 import PropTypes from "prop-types";
-import { createNewComment } from "../../services/index/comment";
-import { useMutation } from "@tanstack/react-query";
-import {useSelector} from "react-redux"
-import {toast} from "react-hot-toast"
-
+import { createNewComment, deleteComment, updateComment } from "../../services/index/comment";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useSelector } from "react-redux";
+import { toast } from "react-hot-toast";
 
 import { CommentForm } from "./commentForm";
-
 
 import { Comment } from "./comment";
 
@@ -15,43 +13,91 @@ export default function CommentsContainer({
   className,
   logginedUSerId,
   comments,
-  postSlug
+  postSlug,
 }) {
+  const queryClient = useQueryClient();
   const [affectedComment, setAffectedComment] = useState(null);
-  const userState=useSelector(state=>state.user)
-
+  const userState = useSelector((state) => state.user);
 
   const { mutate: mutateNewComment, isLoading: isLoadingNewComment } =
     useMutation({
       mutationFn: ({ token, desc, slug, parent, replyOnUser }) => {
         return createNewComment({ token, desc, slug, parent, replyOnUser });
       },
-      onSuccess:()=>{
-        toast.success("your comment is sent successfully , it will be  visible after confirmation of admin")
+      onSuccess: () => {
+        toast.success(
+          "your comment is sent successfully , it will be  visible after confirmation of admin"
+        );
+        queryClient.invalidateQueries(["blog",postSlug])
       },
-      onError:(error)=>{
-        toast.error(error.message)
+      onError: (error) => {
+        toast.error(error.message);
         console.log(error);
-      }
+      },
     });
 
-    useEffect(() => {
-      setAffectedComment(null);
-    }, []);
-    
+  // update comment
+  const { mutate: mutateUpdateComment} =
+    useMutation({
+      mutationFn: ({ token, desc, commentId }) => {
+        return updateComment({ token, desc,  commentId });
+      },
+      onSuccess: () => {
+        toast.success(
+          "your comment  has been updated successfully , it will be  visible after confirmation of admin"
+        );
+      },
+      onError: (error) => {
+        toast.error(error.message);
+        console.log(error);
+      },
+    });
+
+
+
+    // deleteComment 
+    const { mutate: mutatedeleteComment} =
+    useMutation({
+      mutationFn: ({ token, commentId }) => {
+        return deleteComment({ token,commentId  });
+      },
+      onSuccess: () => {
+        toast.success(
+          "your comment  has been deleted successfully "
+        );
+        queryClient.invalidateQueries(["blog",postSlug])
+      },
+      onError: (error) => {
+        toast.error(error.message);
+        console.log(error);
+      },
+    });
+
+  useEffect(() => {
+    setAffectedComment(null);
+  }, []);
 
   const addCommentHandler = (value, parent = null, replyOnUser = null) => {
-    mutateNewComment({ desc: value, slug:postSlug,parent, replyOnUser ,token:userState?.userInfo?.token});
-    setAffectedComment(null)
+    mutateNewComment({
+      desc: value,
+      slug: postSlug,
+      parent,
+      replyOnUser,
+      token: userState?.userInfo?.token,
+    });
+    setAffectedComment(null);
   };
 
   // update your commnet
   const updateCommentHandler = (value, commentId) => {
+    mutateUpdateComment({token:userState?.userInfo?.token,desc:value,commentId})
     setAffectedComment(null);
   };
 
   // delete comment
   const deleteCommentHandler = (commentId) => {
+
+    mutatedeleteComment({token:userState?.userInfo?.token,commentId})
     setAffectedComment(null);
   };
 
@@ -65,7 +111,6 @@ export default function CommentsContainer({
         btnLabel="Send"
         formSubmitHandler={(value) => addCommentHandler(value)}
         loading={isLoadingNewComment}
-
       />
       <div className="space-y-4 mt-8">
         {comments.map((comment) => (
@@ -91,5 +136,5 @@ CommentsContainer.propTypes = {
   logginedUSerId: PropTypes.string.isRequired,
   affectedComment: PropTypes.string,
   comments: PropTypes.array,
-  postSlug:PropTypes.string
+  postSlug: PropTypes.string,
 };
--- src/services/index/comment.jsx
+++ src/services/index/comment.jsx
@@ -28,3 +28,53 @@ export const createNewComment = async ({
     throw new Error(error.message);
   }
 };
+
+
+// update comment
+export const updateComment = async ({
+  token,
+  desc,
+commentId
+}) => {
+  try {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    // console.log(desc);
+    const { data } = await axios.put(`http://localhost:4000/api/comments/${commentId}`, {
+      desc
+    },config);
+  
+    return data
+  } catch (error) {
+    if (error.response && error.response.data.message)
+      throw new Error(error.response.data.message);
+    throw new Error(error.message);
+  }
+};
+
+
+// delete comment
+export const deleteComment = async ({
+  token,
+  
+commentId
+}) => {
+  try {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    // console.log(desc);
+    const { data } = await axios.delete(`http://localhost:4000/api/comments/${commentId}` ,config);
+  
+    return data
+  } catch (error) {
+    if (error.response && error.response.data.message)
+      throw new Error(error.response.data.message);
+    throw new Error(error.message);
+  }
+};
